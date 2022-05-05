5a337079d26a8eb13aba48ecd88fa6a5153e711d
gabriel Carino <gabejcarino@gmail.com>
2022-05-05 15:48:32 -0500

    commit tree 71488efdae2f971b15367108be8add4f89192a8b

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, onRemoveTask }) {
+  const taskList = tasks.map((task) => (
+    <Task
+      key={task.text}
+      text={task.text}
+      category={task.category}
+      onRemoveTask={onRemoveTask}
+    />
+  ));
+  return <div className="tasks">{taskList}</div>;
 }
 
 export default TaskList;
